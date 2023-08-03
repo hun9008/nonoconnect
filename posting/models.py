bc7cd1d01e1c60e6b9b8d328575963eed77d3b03
cheonha <cjsgksla@ajou.ac.kr>
2023-08-03 13:56:43 +0900

    Feature posting model 생성

--- posting/models.py
+++ posting/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Feed(models.Model):
     feed_id = models.BigAutoField(help_text="Feed ID", primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='feed_user_id') # FK, related_name을 id값으로 식별
     title = models.CharField(max_length=100)
     context = models.TextField(max_length=2000, null=True)  
     created_date = models.DateTimeField(auto_now_add=True) # 레코드 추가 시 자동 추가
@@ -15,16 +16,16 @@ class Feed(models.Model):
         db_table = 'Feed'
 
 class Feed_image(models.Model):
-    feed = models.ForeignKey(Feed, on_delete=models.CASCADE) # 다대일 관계 지정
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE,related_name='image_feed_id') # 다대일 관계 지정
     image = models.ImageField(default='media/posting/default_image.jpeg', upload_to='posting', blank=True, null=True)
 
     class Meta:
         db_table = 'Feed_image'
 
 class Comment(models.Model):
-    feed = models.ForeignKey(Feed, on_delete=models.CASCADE) # 다대일 관계 지정
+    feed_id = models.ForeignKey(Feed, on_delete=models.CASCADE, related_name='comment_feed_id') # 다대일 관계 지정
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_user_id') # FK, related_name을 id값으로 식별
     comment_id = models.BigAutoField(help_text="Comment ID", primary_key=True)
-    # feed_id = models.BigIntegerField() # 내가 어떤 컨텐츠에 대한 댓글이냐
     context = models.TextField(max_length=2000)  
     created_date = models.DateTimeField(auto_now_add=True) # 레코드 추가 시 자동 추가
     updated_date = models.DateTimeField(auto_now=True) # 레코드 변경 시 자동추가
@@ -33,7 +34,7 @@ class Comment(models.Model):
         db_table = 'Comment'
 
 class Comment_image(models.Model):
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE) # 다대일 관계 지정
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE,related_name='image_comment_id') # 다대일 관계 지정
     image = models.ImageField(default='media/posting/default_image.jpeg', upload_to='posting', blank=True, null=True)
 
     class Meta:
--- posting/views.py
+++ posting/views.py
@@ -1,3 +1,58 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from .models import Feed, Comment, User
 # Create your views here.
+
+class Feed_View_Set(APIView):
+    def post(self, request):
+        user_id = request.data.get("user_id")
+        title = request.data.get('title')
+        context = request.data.get('context')
+        longitude = request.data.get('longitude')
+        latitude = request.data.get('latitude')
+
+        Feed.objects.create(
+            user_id = user_id,
+            title = title,
+            context = context,
+            status = True,
+            longitude = longitude,
+            latitude = latitude )
+        
+        return JsonResponse({"message": "Feed created successfully."})
+    
+    def get(self, request):
+        # session에 user_id값이 없으면 로그인페이지 라우팅
+        user_id = request.session.get('user_id', None)
+        
+        if user_id is None:
+            return render(request, "user/login.html")
+    
+        user = User.objects.filter(user_id = user_id).first()
+
+        feed_object_list = Feed.objects.all().order_by("-feed_id") #모든 피드를 다가져온다.
+        feed_list = []
+        for feed in feed_object_list: #피드를 하나씩 꺼내서
+            user = User.objects.filter(user_id= feed.user_id).first() #피드를 쓴 user의 정보
+            comment_object_list = Comment.objects.filter(feed_id=feed.feed_id) #피드에 달린 댓글 전부가져오기
+            comment_list = []
+            for comment in comment_object_list:
+                user = User.objects.filter(user_id=comment.user_id).first() # 댓글을 쓴 유저 email 가져오기
+                comment_list.append(dict(feed_id = comment.feed_id,
+                                       user_id=user.user_id,
+                                       comment_id=comment.comment_id,
+                                       user_nickname = user.nickname,
+                                       context = comment.context,
+                                       ))
+            
+            feed_list.append(dict(id=feed.feed_id,
+                                  context=feed.context,
+                                  nickname=user.nickname,
+                                  comment_list=comment_list,
+                                  ))
+        
+        
+        
+        return JsonResponse({"feeds": feed_list, "user": user.user_id})
+
